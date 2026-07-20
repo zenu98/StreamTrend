c4821764a18587feffb091c0dc4eb72547ca2720
Seo <zenu9876@gmail.com>
2026-07-20 22:57:20 +0900

    layout fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,9 +55,9 @@ export default function RootLayout({
       lang="ko"
       className={`dark ${geistSans.variable} ${geistMono.variable} ${anton.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col ">
         <Header />
-        <main className="flex-1">{children}</main>
+        {children}
         <Footer />
         <Analytics />
       </body>
